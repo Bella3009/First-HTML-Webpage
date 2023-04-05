f7cff1ca639519372ce41dee2c2c1c5f7ccdbecd
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-04-05 20:08:44 +0200

    Update Main.py
    Add other filter/webpages for viewers to view the data.

--- Main.py
+++ Main.py
@@ -20,6 +20,22 @@ def dt(station, date):
     return {"station": station,
                  "date": date,
                  "temperature": temperature}
+@app.route("/api/v1/<station>")
+def statonly(station):
+    
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    data = pd.read_csv(filename, skiprows = 20, parse_dates=["    DATES"])
+    result = data.to_dict(orient="records")
+    return result
+	
+@app.route("/api/v1/yearly/<station>/<year>")
+def dt(station, year):
+    
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    data = pd.read_csv(filename, skiprows = 20)
+    data["    DATES"] = data["    DATES"].astype(str)
+    result = data[data["    DATES"].str.startswith(str(year))]
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
